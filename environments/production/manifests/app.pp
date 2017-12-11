bf72c526819c09cbb7e0822298e88b50330cc740
Mario Adrián Domínguez González <tupacsoul@gmail.com>
2017-12-11 10:49:36 +0100

    Test parametrización

--- environments/production/manifests/app.pp
+++ environments/production/manifests/app.pp
@@ -1,6 +1,11 @@
 node 'puppetnode1', 'puppetnode2', 'puppetnode5.localdomain' {
+	# Si usamos una clase sin parámetros, simplemente importamos.
 	include base
 	include java::install
-	include tomcat
+
+	# Si usamos una clase con parámetros, tendremos que llamarla como clase y sus parámetros.
+	class { '::tomcat':
+		$shutdown_verbose	=> true,
+	}
 
 }
