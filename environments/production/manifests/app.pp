26eb7849e8157ad1bb196f9bf3b583c42f8d1372
Mario Adrián Domínguez González <tupacsoul@gmail.com>
2017-12-11 15:10:27 +0100

    Added checksum

--- environments/production/manifests/app.pp
+++ environments/production/manifests/app.pp
@@ -10,6 +10,7 @@ node 'puppetnode1', 'puppetnode2', 'puppetnode5.localdomain' {
 
 	tomcat::deploy {'sysfoo':
 		deploy_url	=> 'https://2-113859142-gh.circle-artifacts.com/0/tmp/circle-artifacts.btdOoSb/sysfoo.war',
+		checksum_value => 'a60dcd4d864302bf4fa40588dfecaa0e',
 	}
 
 }
